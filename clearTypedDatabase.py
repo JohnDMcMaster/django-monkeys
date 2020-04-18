a577d76ab27b5537ca8001563fdea35ed2b6eb80
John McMaster <johndmcmaster@gmail.com>
2020-04-18 01:10:07 -0700

    clear database test
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- clearTypedDatabase.py
+++ clearTypedDatabase.py
@@ -4,16 +4,18 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "monkeys.settings")
 import django
 django.setup()
 
-from typer.models import Die, DieImage, TypedDie
+from typer.models import Die, DieInstructionsImage, DieImage, TypedDie
 
 Die.objects.all().delete()
+# DieInstructionsImage.all().delete()
 DieImage.objects.all().delete()
 TypedDie.objects.all().delete()
 
 
-from typer.models import Pdf, PdfImage, TypedPdf
+from typer.models import Pdf, PdfInstructionsImage, PdfImage, TypedPdf
 
 Pdf.objects.all().delete()
+# PdfInstructionsImage.all().delete()
 PdfImage.objects.all().delete()
 TypedPdf.objects.all().delete()
 
